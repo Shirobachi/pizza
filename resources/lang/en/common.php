2f9375bb5eb4253a0fea4aa45e41d49c8a1ee8cb
Simon Hryszko <simon@hryszko.dev>
2021-07-30 05:23:47 +0200

    Added missed langs keys

--- resources/lang/en/common.php
+++ resources/lang/en/common.php
@@ -7,6 +7,6 @@ return [
   'noListing' => 'No found any record',
   'callNow' => 'Order now!',
   'noIngredients' => 'No any ingredients for this pizza',
-  '' => '',
-
+  'errorsTitle' => 'Holy guacamole!',
+  'errorsDesc' => 'You should check in on some of those fields below.',
 ];
